7d161a14459e2a88a65480517a2acf360ecedb9f
yami_beta <yami-beta@users.noreply.github.com>
2016-09-22 16:17:47 +0900

    Update test to use async function
    Update babel preset ("es2015" => "latest")

--- test/license-list.js
+++ test/license-list.js
@@ -1,41 +1,31 @@
+import 'babel-polyfill';
 import assert from 'assert';
 import licenseList from '../src/index';
 
 describe('license-list', function () {
   let pkgInfo;
-  before(() => {
-    pkgInfo = licenseList('.', { dev: false }).then((packages) => {
-      return packages['license-list@0.0.1'];
-    });
+  before(async () => {
+    const packages = await licenseList('.', { dev: false });
+    pkgInfo = packages['license-list@0.0.1'];
   });
 
-  it('should return name ("license-list")', () => {
-    return pkgInfo.then((info) => {
-      assert.equal(info.name, 'license-list');
-    });
+  it("should return package's name", () => {
+    assert.equal(pkgInfo.name, 'license-list');
   });
 
-  it('should return version ("0.0.1")', () => {
-    return pkgInfo.then((info) => {
-      assert.equal(info.version, '0.0.1');
-    });
+  it("should return package's version", () => {
+    assert.equal(pkgInfo.version, '0.0.1');
   });
 
-  it('should return license ("MIT")', () => {
-    return pkgInfo.then((info) => {
-      assert.equal(info.license, 'MIT');
-    });
+  it("should return package's license", () => {
+    assert.equal(pkgInfo.license, 'MIT');
   });
 
-  it('should return maintainers (undefined)', () => {
-    return pkgInfo.then((info) => {
-      assert.equal(info.maintainers, undefined);
-    });
+  it("should return package's maintainers", () => {
+    assert.equal(pkgInfo.maintainers, undefined);
   });
 
-  it('should return contributors (undefined)', () => {
-    return pkgInfo.then((info) => {
-      assert.equal(info.contributors, undefined);
-    });
+  it("should return package's contributors", () => {
+    assert.equal(pkgInfo.contributors, undefined);
   });
 });
