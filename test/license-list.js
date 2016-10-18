8bd35f7cd1234a9f18d94218b334e7d4b747ef56
yami_beta <yami-beta@users.noreply.github.com>
2016-10-19 01:04:50 +0900

    Use package.json for test
    Because I must update test code until now when package's version is updated.

--- test/license-list.js
+++ test/license-list.js
@@ -1,24 +1,25 @@
 import 'babel-polyfill';
 import assert from 'assert';
 import licenseList from '../src/index';
+import packageJson from '../package.json';
 
 describe('license-list', function () {
   let pkgInfo;
   before(async () => {
     const packages = await licenseList('.', { dev: false });
-    pkgInfo = packages['license-list@0.0.3'];
+    pkgInfo = packages[`license-list@${packageJson.version}`];
   });
 
   it("should return package's name", () => {
-    assert.equal(pkgInfo.name, 'license-list');
+    assert.equal(pkgInfo.name, packageJson.name);
   });
 
   it("should return package's version", () => {
-    assert.equal(pkgInfo.version, '0.0.3');
+    assert.equal(pkgInfo.version, packageJson.version);
   });
 
   it("should return package's license", () => {
-    assert.equal(pkgInfo.license, 'MIT');
+    assert.equal(pkgInfo.license, packageJson.license);
   });
 
   it("should return package's maintainers", () => {
