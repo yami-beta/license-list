4c8fd89ac89268ea56a23b865406f5bbff470b80
yami_beta <yami-beta@users.noreply.github.com>
2016-10-25 09:41:37 +0900

    Fix eslint warning

--- src/bin/cmd.js
+++ src/bin/cmd.js
@@ -7,7 +7,7 @@ const formatOutput = (pkgInfo) => {
   const licenseFile = (() => {
     if (typeof pkgInfo.licenseFile !== 'string') { return '  LICENSE file is not exist'; }
     // Append indent (two speces) to each line
-    return pkgInfo.licenseFile.split(/\n/).map((line) => `  ${line}`).join("\n");
+    return pkgInfo.licenseFile.split(/\n/).map((line) => `  ${line}`).join('\n');
   })();
   const output = `${pkgInfo.name}@${pkgInfo.version} (${pkgInfo.license})
 
@@ -26,8 +26,8 @@ const argv = minimist(process.argv.slice(2), {
     dev: false,
   },
 });
-const baseDir = argv['_'][0] || '.';
-const option = { dev: argv['dev'] };
+const baseDir = argv._[0] || '.';
+const option = { dev: argv.dev };
 licenseList(baseDir, option).then((packages) => {
   Object.keys(packages).forEach((pkgId) => {
     console.log(formatOutput(packages[pkgId]));
